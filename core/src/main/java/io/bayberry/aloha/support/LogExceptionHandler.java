b31616cd282532bf7ed958d25ce20a8b4cde73d7
Pojo Zhang <pojo.zhang@gmail.com>
2017-06-08 01:17:57 +0800

    introduce slf4j

--- core/src/main/java/io/bayberry/aloha/support/LogExceptionHandler.java
+++ core/src/main/java/io/bayberry/aloha/support/LogExceptionHandler.java
@@ -2,11 +2,15 @@ package io.bayberry.aloha.support;
 
 import io.bayberry.aloha.EventBus;
 import io.bayberry.aloha.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LogExceptionHandler implements ExceptionHandler {
 
+    private Logger log = LoggerFactory.getLogger(LogExceptionHandler.class);
+
     @Override
     public void handle(String channel, Object event, EventBus eventBus, Exception exception) {
-        System.out.print(exception.getMessage());
+        log.error("An error occurs:", exception);
     }
 }
