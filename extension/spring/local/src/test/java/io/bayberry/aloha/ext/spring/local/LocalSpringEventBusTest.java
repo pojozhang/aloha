3daed30a539408b920f803cda22fac8004d816e1
Jiaan Zhang <jiaan.zhang@baidao.com>
2017-06-28 13:28:33 +0800

    refactor test

--- extension/spring/local/src/test/java/io/bayberry/aloha/ext/spring/local/LocalSpringEventBusTest.java
+++ extension/spring/local/src/test/java/io/bayberry/aloha/ext/spring/local/LocalSpringEventBusTest.java
@@ -1,26 +1,17 @@
 package io.bayberry.aloha.ext.spring.local;
 
-import io.bayberry.aloha.EventBus;
 import io.bayberry.aloha.test.Subscriber;
-import io.bayberry.aloha.test.SyncEvent;
 import io.bayberry.aloha.test.spring.BaseSpringTest;
 import org.junit.Before;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
-import java.util.concurrent.CountDownLatch;
-
-import static org.junit.Assert.assertEquals;
 
 @SpringBootApplication
 public class LocalSpringEventBusTest extends BaseSpringTest {
 
     @Autowired
     private ApplicationContext applicationContext;
-    private Subscriber subscriber;
-    private EventBus eventBus;
 
     @Before
     public void setUp() {
@@ -28,22 +19,5 @@ public class LocalSpringEventBusTest extends BaseSpringTest {
         this.eventBus = new LocalSpringEventBus(this.applicationContext);
         this.eventBus.register(subscriber);
         this.eventBus.start();
-    }
-
-    @Test
-    public void the_subscriber_should_be_called_synchronously_after_single_event_is_post() {
-        this.subscriber.countDownLatch = new CountDownLatch(1);
-        this.eventBus.post(new SyncEvent());
-        assertEquals(0, subscriber.countDownLatch.getCount());
-    }
-
-    @Test
-    public void the_subscriber_should_be_called_synchronously_for_n_times_after_n_events_are_post() {
-        final int NUMBER = 5;
-        this.subscriber.countDownLatch = new CountDownLatch(NUMBER);
-        for (int i = 0; i < NUMBER; i++) {
-            this.eventBus.post(new SyncEvent());
-        }
-        assertEquals(0, subscriber.countDownLatch.getCount());
     }
 }
--- test/spring/src/main/java/io/bayberry/aloha/test/spring/BaseSpringTest.java
+++ test/spring/src/main/java/io/bayberry/aloha/test/spring/BaseSpringTest.java
@@ -1,10 +1,12 @@
 package io.bayberry.aloha.test.spring;
 
+import io.bayberry.aloha.test.BaseTestCase;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class BaseSpringTest {
+public abstract class BaseSpringTest extends BaseTestCase {
+
 }
