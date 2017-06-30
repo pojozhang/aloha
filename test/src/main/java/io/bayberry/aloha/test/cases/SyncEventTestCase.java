9f81f0e3670be92539dc603719c0399e3577d380
POJO <pojo.zhang@gmail.com>
2017-06-30 19:48:55 +0800

    add test

--- test/src/main/java/io/bayberry/aloha/test/cases/SyncEventTestCase.java
+++ test/src/main/java/io/bayberry/aloha/test/cases/SyncEventTestCase.java
@@ -3,6 +3,7 @@ package io.bayberry.aloha.test.cases;
 import static org.junit.Assert.assertEquals;
 
 import io.bayberry.aloha.test.BaseTest;
+import io.bayberry.aloha.test.SubSyncEvent;
 import io.bayberry.aloha.test.SyncEvent;
 import java.util.concurrent.CountDownLatch;
 import org.junit.Test;
@@ -23,6 +24,13 @@ public abstract class SyncEventTestCase extends BaseTest {
         for (int i = 0; i < EVENT_NUMBER; i++) {
             this.eventBus.post(new SyncEvent());
         }
+        assertEquals(0, subscriber.countDownLatch.getCount());
+    }
+
+    @Test
+    public void the_subscriber_should_be_called_synchronously_if_target_event_is_base_type_of_source_event() {
+        this.subscriber.countDownLatch = new CountDownLatch(1);
+        this.eventBus.post(new SubSyncEvent());
         assertEquals(0, subscriber.countDownLatch.getCount());
     }
 }
