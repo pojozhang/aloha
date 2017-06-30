e94abf5ec2512a38c21055917dbefea7a441ed71
Jiaan Zhang <pojo.zhang@gmail.com>
2017-06-30 09:48:47 +0800

    add channel

---
+++ core/src/main/java/io/bayberry/aloha/Channel.java
@@ -0,0 +1,21 @@
+package io.bayberry.aloha;
+
+public class Channel {
+
+    private String name;
+
+    public Channel() {
+    }
+
+    public Channel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
