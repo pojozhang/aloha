7e981fe8ede015c6dd2f18b7464716ba9bae9692
Pojo Zhang <pojo.zhang@gmail.com>
2017-07-01 23:46:25 +0800

    update reflection util

--- core/src/main/java/io/bayberry/aloha/support/GenericSubscriberResolver.java
+++ core/src/main/java/io/bayberry/aloha/support/GenericSubscriberResolver.java
@@ -6,6 +6,7 @@ import io.bayberry.aloha.Subscriber;
 import io.bayberry.aloha.SubscriberResolver;
 import io.bayberry.aloha.annotation.Subscribe;
 import io.bayberry.aloha.util.Reflection;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,16 +14,16 @@ public class GenericSubscriberResolver implements SubscriberResolver {
 
     @Override
     public List<Subscriber> resolve(Object target, EventBus eventBus) {
-        return new Reflection().getMethodsWithAnnotation(target, Subscribe.class)
-            .map(method -> {
-                Subscribe subscribe = method.getAnnotation(Subscribe.class);
-                Channel channel = new Channel(subscribe.channel());
-                if (channel.getName() == null || channel.getName().length() == 0) {
-                    channel = eventBus.getChannelResolver().resolve(method.getParameterTypes()[0]);
-                }
-                return new GenericSubscriber(channel, target, method, eventBus,
-                    eventBus.getExceptionHandlerFactory().provide(subscribe.exceptionHandler()),
-                    eventBus.getExecutionStrategyFactory().provide(subscribe.executionStrategy()));
-            }).collect(Collectors.toList());
+        return new Reflection().getMethodsWithAnnotation(target.getClass(), Subscribe.class)
+                .map(method -> {
+                    Subscribe subscribe = method.getAnnotation(Subscribe.class);
+                    Channel channel = new Channel(subscribe.channel());
+                    if (channel.getName() == null || channel.getName().length() == 0) {
+                        channel = eventBus.getChannelResolver().resolve(method.getParameterTypes()[0]);
+                    }
+                    return new GenericSubscriber(channel, target, method, eventBus,
+                            eventBus.getExceptionHandlerFactory().provide(subscribe.exceptionHandler()),
+                            eventBus.getExecutionStrategyFactory().provide(subscribe.executionStrategy()));
+                }).collect(Collectors.toList());
     }
 }
--- core/src/main/java/io/bayberry/aloha/util/Reflection.java
+++ core/src/main/java/io/bayberry/aloha/util/Reflection.java
@@ -3,12 +3,13 @@ package io.bayberry.aloha.util;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Reflection {
 
-    public Stream<Method> getMethodsWithAnnotation(Object target, Class<? extends Annotation> annotationType) {
-        Method[] methods = target.getClass().getMethods();
+    public Stream<Method> getMethodsWithAnnotation(Class targetClass, Class<? extends Annotation> annotationType) {
+        Method[] methods = targetClass.getMethods();
         return Arrays.stream(methods)
                 .filter(method -> method.isAnnotationPresent(annotationType));
     }
