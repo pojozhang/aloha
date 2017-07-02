7622887f840f76c0e249e2ffddffc4de3641d9d0
Pojo Zhang <pojo.zhang@gmail.com>
2017-07-02 23:01:22 +0800

    refactor SpringEventProxy

--- extension/spring/local/src/main/java/io/bayberry/aloha/ext/spring/local/SpringEventProxy.java
+++ extension/spring/local/src/main/java/io/bayberry/aloha/ext/spring/local/SpringEventProxy.java
@@ -7,6 +7,12 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.PayloadApplicationEvent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class SpringEventProxy implements ApplicationListener {
 
     private EventBus eventBus;
@@ -17,32 +23,32 @@ public class SpringEventProxy implements ApplicationListener {
 
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
-        Channel channel;
         Object source;
         if (event instanceof PayloadApplicationEvent) {
-            channel = resolveChannelsFromPayloadApplicationEvent((PayloadApplicationEvent) event);
             source = ((PayloadApplicationEvent) event).getPayload();
         } else {
-            channel = resolveChannelsFromApplicationEvent(event);
             source = event.getSource();
         }
 
-        Listener listener = this.eventBus.getListenerRegistry().getListener(channel);
-        if (listener == null) {
-            return;
-        }
-        listener.notifyAll(source);
+        this.getCandidateChannels(source.getClass()).forEach(channel -> {
+            Listener listener = this.eventBus.getListenerRegistry().getListener(channel);
+            if (listener == null) {
+                return;
+            }
+            listener.notifyAll(source);
+        });
     }
 
-    private Channel resolveChannelsFromPayloadApplicationEvent(PayloadApplicationEvent event) {
-        return this.resolveChannels(event.getPayload().getClass());
+    //TODO
+    private List<Channel> getCandidateChannels(Class eventType) {
+        List<Channel> channels = new ArrayList<>();
+        channels.add(this.eventBus.getChannelResolver().resolve(eventType));
+        channels.addAll(Arrays.stream(eventType.getInterfaces()).map(this::resolveChannel).collect(toList()));
+        //add all interfaces to channels
+        return channels;
     }
 
-    private Channel resolveChannelsFromApplicationEvent(ApplicationEvent event) {
-        return this.resolveChannels(event.getSource().getClass());
-    }
-
-    private Channel resolveChannels(Class eventType) {
+    private Channel resolveChannel(Class eventType) {
         return this.eventBus.getChannelResolver().resolve(eventType);
     }
 }
