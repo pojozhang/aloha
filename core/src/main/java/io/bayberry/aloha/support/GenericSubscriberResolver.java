71b3e1a9e9cb94063cc95f318f52318ddd497381
POJO <pojo.zhang@gmail.com>
2017-07-03 13:38:07 +0800

    refactor Reflection util

--- core/src/main/java/io/bayberry/aloha/support/GenericSubscriberResolver.java
+++ core/src/main/java/io/bayberry/aloha/support/GenericSubscriberResolver.java
@@ -14,16 +14,16 @@ public class GenericSubscriberResolver implements SubscriberResolver {
 
     @Override
     public List<Subscriber> resolve(Object target, EventBus eventBus) {
-        return new Reflection().getMethodsWithAnnotation(target.getClass(), Subscribe.class)
-                .map(method -> {
-                    Subscribe subscribe = method.getAnnotation(Subscribe.class);
-                    Channel channel = new Channel(subscribe.channel());
-                    if (channel.getName() == null || channel.getName().length() == 0) {
-                        channel = eventBus.getChannelResolver().resolve(method.getParameterTypes()[0]);
-                    }
-                    return new GenericSubscriber(channel, target, method, eventBus,
-                            eventBus.getExceptionHandlerFactory().provide(subscribe.exceptionHandler()),
-                            eventBus.getExecutionStrategyFactory().provide(subscribe.executionStrategy()));
-                }).collect(Collectors.toList());
+        return new Reflection().getMethodsWithAnnotation(target.getClass(), Subscribe.class).stream()
+            .map(method -> {
+                Subscribe subscribe = method.getAnnotation(Subscribe.class);
+                Channel channel = new Channel(subscribe.channel());
+                if (channel.getName() == null || channel.getName().length() == 0) {
+                    channel = eventBus.getChannelResolver().resolve(method.getParameterTypes()[0]);
+                }
+                return new GenericSubscriber(channel, target, method, eventBus,
+                    eventBus.getExceptionHandlerFactory().provide(subscribe.exceptionHandler()),
+                    eventBus.getExecutionStrategyFactory().provide(subscribe.executionStrategy()));
+            }).collect(Collectors.toList());
     }
 }
