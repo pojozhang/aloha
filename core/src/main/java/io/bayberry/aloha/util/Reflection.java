bc9021d8c714592a89aa29c5db8a7743125198b2
POJO <pojo.zhang@gmail.com>
2017-07-03 13:29:16 +0800

    fix ConcurrentModificationException issue

--- core/src/main/java/io/bayberry/aloha/util/Reflection.java
+++ core/src/main/java/io/bayberry/aloha/util/Reflection.java
@@ -1,25 +1,24 @@
 package io.bayberry.aloha.util;
 
+import static java.util.stream.Collectors.toSet;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toSet;
-
 public class Reflection {
 
     public Stream<Method> getMethodsWithAnnotation(Class targetClass, Class<? extends Annotation> annotationType) {
         Method[] methods = targetClass.getMethods();
         return Arrays.stream(methods)
-                .filter(method -> method.isAnnotationPresent(annotationType));
+            .filter(method -> method.isAnnotationPresent(annotationType));
     }
 
-    //TODO fix java.util.ConcurrentModificationException issue
     public Set<Class> getAllInterfaces(Class targetClass) {
         Set<Class> interfaces = Arrays.stream(targetClass.getInterfaces()).collect(toSet());
-        interfaces.stream().map(this::getAllInterfaces).forEach(interfaces::addAll);
+        interfaces.forEach(i -> interfaces.addAll(this.getAllInterfaces(i)));
         return interfaces;
     }
 }
