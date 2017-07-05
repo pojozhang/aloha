54d1de33e9796a20a2fa019abc77b487a95281c7
POJO <pojo.zhang@gmail.com>
2017-07-05 17:38:29 +0800

    shutdown listener synchronously

--- core/src/main/java/io/bayberry/aloha/support/AsyncListenerDecorator.java
+++ core/src/main/java/io/bayberry/aloha/support/AsyncListenerDecorator.java
@@ -2,13 +2,15 @@ package io.bayberry.aloha.support;
 
 import io.bayberry.aloha.Listener;
 import io.bayberry.aloha.exception.AlohaException;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class AsyncListenerDecorator extends DelegateListener {
 
     private ExecutorService threadPool = Executors.newSingleThreadExecutor();
+    private Future task;
 
     public AsyncListenerDecorator(Listener delegate) {
         super(delegate);
@@ -16,7 +18,7 @@ public class AsyncListenerDecorator extends DelegateListener {
 
     @Override
     public void start() {
-        this.threadPool.execute(() -> {
+        this.task = this.threadPool.submit(() -> {
             try {
                 super.start();
             } catch (Exception exception) {
@@ -31,7 +33,12 @@ public class AsyncListenerDecorator extends DelegateListener {
 
     @Override
     public void shutdown() {
+        this.task.cancel(true);
         this.threadPool.shutdown();
+        try {
+            this.threadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException ignored) {
+        }
         super.shutdown();
     }
 }
