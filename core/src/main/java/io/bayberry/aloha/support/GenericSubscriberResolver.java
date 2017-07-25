b6c426d49238ca706e50df3f0b8572fdddc1a92a
Pojo Zhang <pojo.zhang@gmail.com>
2017-07-25 22:25:52 +0800

    refactor

--- core/src/main/java/io/bayberry/aloha/support/GenericSubscriberResolver.java
+++ core/src/main/java/io/bayberry/aloha/support/GenericSubscriberResolver.java
@@ -7,6 +7,7 @@ import io.bayberry.aloha.SubscriberResolver;
 import io.bayberry.aloha.annotation.Subscribe;
 import io.bayberry.aloha.util.Reflection;
 
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,15 +16,20 @@ public class GenericSubscriberResolver implements SubscriberResolver {
     @Override
     public List<Subscriber> resolve(Object target, MessageBus messageBus) {
         return new Reflection().getMethodsWithAnnotation(target.getClass(), Subscribe.class).stream()
-            .map(method -> {
-                Subscribe subscribe = method.getAnnotation(Subscribe.class);
-                Channel channel = new Channel(subscribe.channel());
-                if (channel.getName() == null || channel.getName().length() == 0) {
-                    channel = messageBus.getChannelResolver().resolve(method.getParameterTypes()[0]);
-                }
-                return new GenericSubscriber(channel, target, method, messageBus,
-                    messageBus.getExceptionHandlerFactory().provide(subscribe.exceptionHandler()),
-                    messageBus.getExecutionStrategyFactory().provide(subscribe.executionStrategy()));
-            }).collect(Collectors.toList());
+                .map(method -> {
+                    Subscribe subscribe = method.getAnnotation(Subscribe.class);
+                    Channel channel = getResolvedChannel(subscribe, method, messageBus);
+                    return new GenericSubscriber(channel, target, method, messageBus,
+                            messageBus.getExceptionHandlerFactory().provide(subscribe.exceptionHandler()),
+                            messageBus.getExecutionStrategyFactory().provide(subscribe.executionStrategy()));
+                }).collect(Collectors.toList());
+    }
+
+    protected Channel getResolvedChannel(Subscribe subscribe, Method method, MessageBus messageBus) {
+        Channel channel = new Channel(subscribe.channel());
+        if (channel.getName() == null || channel.getName().length() == 0) {
+            channel = messageBus.getChannelResolver().resolve(method.getParameterTypes()[0]);
+        }
+        return channel;
     }
 }
