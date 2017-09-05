dda005add9b996c6fdfea37183334277c4c8e7d3
Pojo Zhang <pojo.zhang@gmail.com>
2017-09-05 23:31:14 +0800

    add redis proxy

---
+++ extension/spring/redis/src/main/java/io/bayberry/aloha/spring/redis/RedisProxyInvocationHandler.java
@@ -0,0 +1,12 @@
+package io.bayberry.aloha.spring.redis;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class RedisProxyInvocationHandler implements InvocationHandler{
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
