d84be48f76d6bc0e78f965b172cf50dad3021d5c
Pojo Zhang <pojo.zhang@gmail.com>
2017-09-24 23:19:08 +0800

    update assert util

--- core/src/main/java/io/bayberry/aloha/util/Assert.java
+++ core/src/main/java/io/bayberry/aloha/util/Assert.java
@@ -3,6 +3,7 @@ package io.bayberry.aloha.util;
 import io.bayberry.aloha.exception.AlohaException;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public class Assert {
 
@@ -12,6 +13,11 @@ public class Assert {
 
     public static void notNull(Object object, String message) {
         if (object == null) throwException(message);
+    }
+
+    public static <T> T notNull(Optional<T> optional, String message) {
+        if (!optional.isPresent()) throwException(message);
+        return optional.get();
     }
 
     public static void isTrue(boolean condition, String message) {
