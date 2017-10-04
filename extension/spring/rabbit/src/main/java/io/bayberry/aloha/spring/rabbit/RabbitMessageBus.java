01074a9f4cb7aa822a4535ee646082ffa7b45d65
Pojo Zhang <pojo.zhang@gmail.com>
2017-10-04 16:38:33 +0800

    fix compile

--- extension/spring/rabbit/src/main/java/io/bayberry/aloha/spring/rabbit/RabbitMessageBus.java
+++ extension/spring/rabbit/src/main/java/io/bayberry/aloha/spring/rabbit/RabbitMessageBus.java
@@ -1,11 +1,32 @@
 package io.bayberry.aloha.spring.rabbit;
 
-import io.bayberry.aloha.*;
+import io.bayberry.aloha.Listener;
+import io.bayberry.aloha.Message;
+import io.bayberry.aloha.Stream;
+import io.bayberry.aloha.RemoteMessageBus;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.ApplicationContext;
 
 public class RabbitMessageBus extends RemoteMessageBus {
 
+    private ApplicationContext applicationContext;
+    private ConnectionFactory connectionFactory;
+
+    public RabbitMessageBus(ApplicationContext applicationContext, ConnectionFactory connectionFactory) {
+        this.applicationContext = applicationContext;
+        this.connectionFactory = connectionFactory;
+        this.onCreate();
+    }
+
     @Override
-    protected Receiver bindReceiver(Listener listener) {
+    protected void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected Stream bindStream(Listener listener) {
         return null;
     }
 
