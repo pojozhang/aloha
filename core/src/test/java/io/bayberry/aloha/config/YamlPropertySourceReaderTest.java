bdb87c31a3f0c0a1cef5d0dd15091feb8a075379
Pojo Zhang <pojo.zhang@gmail.com>
2017-11-08 23:49:44 +0800

    add test

---
+++ core/src/test/java/io/bayberry/aloha/config/YamlPropertySourceReaderTest.java
@@ -0,0 +1,24 @@
+package io.bayberry.aloha.config;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class YamlPropertySourceReaderTest {
+
+    private PropertySourceReader reader;
+
+    @Before
+    public void setUp() {
+        reader = new YamlPropertySourceReader();
+    }
+
+    @Test
+    public void should_read_property_source_from_yaml_file_properly() {
+        PropertySource propertySource = reader.read(this.getClass().getClassLoader().getResourceAsStream("aloha.yml"));
+        assertTrue(propertySource.get("aloha") instanceof Map);
+    }
+}
