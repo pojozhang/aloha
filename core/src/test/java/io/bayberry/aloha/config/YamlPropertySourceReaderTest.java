61e455a0c9e41b9c87b321e6847991b4283365e3
Pojo Zhang <pojo.zhang@gmail.com>
2017-11-09 22:42:21 +0800

    rename

--- core/src/test/java/io/bayberry/aloha/config/YamlPropertySourceReaderTest.java
+++ core/src/test/java/io/bayberry/aloha/config/YamlPropertySourceReaderTest.java
@@ -18,7 +18,7 @@ public class YamlPropertySourceReaderTest {
 
     @Test
     public void should_read_property_source_from_yaml_file_properly() {
-        PropertySource propertySource = reader.read(this.getClass().getClassLoader().getResourceAsStream("aloha.yml"));
+        PropertySource propertySource = reader.read(this.getClass().getClassLoader().getResourceAsStream("aloha-test.yml"));
         assertTrue(propertySource.get("aloha") instanceof Map);
     }
 }
