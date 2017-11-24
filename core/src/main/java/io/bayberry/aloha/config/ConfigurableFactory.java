39537cd47685ea4e357cad9429f351e7febe997d
Pojo Zhang <pojo.zhang@gmail.com>
2017-11-24 23:41:42 +0800

    rename

--- core/src/main/java/io/bayberry/aloha/config/ConfigurableFactory.java
+++ core/src/main/java/io/bayberry/aloha/config/ConfigurableFactory.java
@@ -2,7 +2,7 @@ package io.bayberry.aloha.config;
 
 import io.bayberry.aloha.MessageBus;
 
-public interface AutoConfigurable<T extends MessageBus> {
+public interface ConfigurableFactory<T extends MessageBus> {
 
     T create(PropertySource propertySource);
 }
--- extension/mqtt/src/main/java/io/bayberry/aloha/mqtt/MqttMessageBusFactory.java
+++ extension/mqtt/src/main/java/io/bayberry/aloha/mqtt/MqttMessageBusFactory.java
@@ -1,9 +1,9 @@
 package io.bayberry.aloha.mqtt;
 
-import io.bayberry.aloha.config.AutoConfigurable;
+import io.bayberry.aloha.config.ConfigurableFactory;
 import io.bayberry.aloha.config.PropertySource;
 
-public class MqttAutoConfiguration implements AutoConfigurable<MqttMessageBus> {
+public class MqttMessageBusFactory implements ConfigurableFactory<MqttMessageBus> {
 
     @Override
     public MqttMessageBus create(PropertySource propertySource) {
